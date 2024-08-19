9eb40c67a21865c179e07ad7fbb8ed763ab13b6f
jsmu <87242098+Mu-James@users.noreply.github.com>
2024-08-18 20:09:55 -0700

    Implement repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, numTimes) {
+    if (numTimes < 0) {
+        return 'ERROR';
+    }
 
+    let result = "";
+    for (let i = 0; i < numTimes; i++) {
+        result = result + str;
+    }
+    return result;
 };
 
 // Do not edit below this line
