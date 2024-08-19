01dbea43947458b075b562a1feb2605fbed4c1ff
jsmu <87242098+Mu-James@users.noreply.github.com>
2024-08-18 20:43:05 -0700

    Implement sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    if ((typeof numOne != 'number' || typeof numTwo != 'number') ||
+        (numOne < 0 || numTwo < 0) || 
+        (!Number.isInteger(numOne) || !Number.isInteger(numTwo))) {
+        return 'ERROR';
+    }
 
+    let result = 0;
+    const minNum = Math.min(numOne, numTwo);
+    const maxNum = Math.max(numOne, numTwo);
+    for (let i = minNum; i <= maxNum; i++) {
+        result = result + i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
